d1ba09e0bd1d218af3072aa59d982cfbb2e5151a
mchu-1999 <jerry.ee06@nctu.edu.tw>
2026-01-09 23:16:44 +0100

    make a script to run barn only

--- scripts/run_barn_nomesh.py
+++ scripts/run_barn_nomesh.py
@@ -1,7 +1,9 @@
 import os
 
-scenes = ['Courthouse', 'Truck', 'Caterpillar', 'Barn', 'Meetingroom', 'Ignatius']
-data_devices = ['cuda', 'cuda', 'cuda', 'cuda', 'cuda', 'cuda']
+# scenes = ['Courthouse', 'Truck', 'Caterpillar', 'Barn', 'Meetingroom', 'Ignatius']
+# data_devices = ['cuda', 'cuda', 'cuda', 'cuda', 'cuda', 'cuda']
+scenes = ['Barn']
+data_devices = ['cuda']
 
 data_base_path="/tudelft.net/staff-umbrella/Deep3D/mingchiehhu/TNT_GOF/TrainingSet"
 out_base_path='output_tnt'
@@ -27,7 +29,7 @@ for id, scene in enumerate(scenes):
         sdf_trunc = 0.016
     
     common_args = f"--data_device {data_devices[id]} --num_cluster 1 --use_depth_filter --voxel_size 0.004 --max_depth {depth_trunc} --sdf_trunc {sdf_trunc} --skip_test"
-    cmd = f'CUDA_VISIBLE_DEVICES={gpu_id} python scripts/render_tnt.py -m {out_base_path}/{scene}/{out_name} {common_args}'
+    cmd = f'CUDA_VISIBLE_DEVICES={gpu_id} python scripts/render_tnt_no_mesh.py -m {out_base_path}/{scene}/{out_name} {common_args}'
     print(cmd)
     os.system(cmd)
 
