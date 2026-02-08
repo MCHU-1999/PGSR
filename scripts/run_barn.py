d927e55019c2f478b7d773a5419ba95524b6708d
mchu-1999 <jerry.ee06@nctu.edu.tw>
2026-02-08 16:24:50 +0100

    FU

--- scripts/run_barn.py
+++ scripts/run_barn.py
@@ -4,21 +4,21 @@ scenes = ['Barn']
 data_devices = ['cuda']
 
 data_base_path="/tudelft.net/staff-umbrella/Deep3D/mingchiehhu/TNT_GOF/TrainingSet"
-# out_base_path='output_tnt'
-out_base_path='output_tnt_highres'
+out_base_path='output_tnt'
+# out_base_path='output_tnt_highres'
 out_name='test'
 gpu_id=0
 
 for id, scene in enumerate(scenes):
 
-    # cmd = f'rm -rf {out_base_path}/{scene}/{out_name}/*'
-    # print(cmd)
-    # os.system(cmd)
+    cmd = f'rm -rf {out_base_path}/{scene}/{out_name}/*'
+    print(cmd)
+    os.system(cmd)
     
-    # common_args = f"--quiet -r2 --ncc_scale 0.5 --data_device {data_devices[id]} --densify_abs_grad_threshold 0.0002 --opacity_cull_threshold 0.05 --exposure_compensation"
-    # cmd = f'CUDA_VISIBLE_DEVICES={gpu_id} python train.py -s {data_base_path}/{scene} -m {out_base_path}/{scene}/{out_name} {common_args}'
-    # print(cmd)
-    # os.system(cmd)
+    common_args = f"--quiet -r2 --ncc_scale 0.5 --data_device {data_devices[id]} --densify_abs_grad_threshold 0.0002 --opacity_cull_threshold 0.05 --exposure_compensation"
+    cmd = f'CUDA_VISIBLE_DEVICES={gpu_id} python train.py -s {data_base_path}/{scene} -m {out_base_path}/{scene}/{out_name} {common_args}'
+    print(cmd)
+    os.system(cmd)
 
     # if scene in ['Meetingroom', 'Courthouse']:
     #     depth_trunc = 4.5
