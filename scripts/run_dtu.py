d8a24788d635139854bfd7eb28bd830d8807a5db
mchu-1999 <jerry.ee06@nctu.edu.tw>
2026-03-26 14:09:17 +0100

    WOW run DTU again

--- scripts/run_dtu.py
+++ scripts/run_dtu.py
@@ -1,11 +1,11 @@
 import os
 
 # scenes = [24, 37, 40, 55, 63, 65, 69, 83, 97, 105, 106, 110, 114, 118, 122]
-scenes = [24]
+scenes = [24, 40]
 data_base_path='/tudelft.net/staff-umbrella/Deep3D/mingchiehhu/DTU'
 out_base_path='output_dtu'
 eval_path='dtu_eval'
-out_name='GM1'
+out_name='test'
 gpu_id=0
 
 for scene in scenes:
@@ -22,15 +22,15 @@ for scene in scenes:
     print(cmd)
     os.system(cmd)
 
-    common_args = "--quiet --num_cluster 1 --voxel_size 0.002 --max_depth 5.0"
-    cmd = f'CUDA_VISIBLE_DEVICES={gpu_id} python render.py -m {out_base_path}/dtu_scan{scene}/{out_name} {common_args}'
+    common_args = "--quiet --num_cluster 1 --voxel_size 0.002 --max_depth 5.0 --use_depth_filter --skip_test"
+    cmd = f'CUDA_VISIBLE_DEVICES={gpu_id} python render_tnt.py -m {out_base_path}/dtu_scan{scene}/{out_name} {common_args}'
     print(cmd)
     os.system(cmd)
 
-    cmd = f"CUDA_VISIBLE_DEVICES={gpu_id} python scripts/eval_dtu/evaluate_single_scene.py " + \
-          f"--input_mesh {out_base_path}/dtu_scan{scene}/{out_name}/mesh/tsdf_fusion_post.ply " + \
-          f"--scan_id {scene} --output_dir {out_base_path}/dtu_scan{scene}/{out_name}/mesh " + \
-          f"--mask_dir {data_base_path} " + \
-          f"--DTU {eval_path}"
-    print(cmd)
-    os.system(cmd)
+#     cmd = f"CUDA_VISIBLE_DEVICES={gpu_id} python scripts/eval_dtu/evaluate_single_scene.py " + \
+#           f"--input_mesh {out_base_path}/dtu_scan{scene}/{out_name}/mesh/tsdf_fusion_post.ply " + \
+#           f"--scan_id {scene} --output_dir {out_base_path}/dtu_scan{scene}/{out_name}/mesh " + \
+#           f"--mask_dir {data_base_path} " + \
+#           f"--DTU {eval_path}"
+#     print(cmd)
+#     os.system(cmd)
