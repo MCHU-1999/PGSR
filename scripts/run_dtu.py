e62f1fb57e5aac155e7a9d3eea281e644a16d365
mchu-1999 <jerry.ee06@nctu.edu.tw>
2026-03-27 16:23:06 +0100

    rerun rendering again

--- scripts/run_dtu.py
+++ scripts/run_dtu.py
@@ -9,18 +9,18 @@ out_name='test'
 gpu_id=0
 
 for scene in scenes:
-    cmd = f'rm -rf {out_base_path}/dtu_scan{scene}/{out_name}/*'
-    print(cmd)
-    os.system(cmd)
+#     cmd = f'rm -rf {out_base_path}/dtu_scan{scene}/{out_name}/*'
+#     print(cmd)
+#     os.system(cmd)
 
-    cmd = f'cp -rf {data_base_path}/scan{scene}/sparse/0/* {data_base_path}/scan{scene}/sparse/'
-    print(cmd)
-    os.system(cmd)
+#     cmd = f'cp -rf {data_base_path}/scan{scene}/sparse/0/* {data_base_path}/scan{scene}/sparse/'
+#     print(cmd)
+#     os.system(cmd)
 
-    common_args = "--quiet -r2 --ncc_scale 0.5 --fg_masks masks"
-    cmd = f'CUDA_VISIBLE_DEVICES={gpu_id} python train.py -s {data_base_path}/scan{scene} -m {out_base_path}/dtu_scan{scene}/{out_name} {common_args}'
-    print(cmd)
-    os.system(cmd)
+#     common_args = "--quiet -r2 --ncc_scale 0.5 --fg_masks masks"
+#     cmd = f'CUDA_VISIBLE_DEVICES={gpu_id} python train.py -s {data_base_path}/scan{scene} -m {out_base_path}/dtu_scan{scene}/{out_name} {common_args}'
+#     print(cmd)
+#     os.system(cmd)
 
     common_args = "--quiet --num_cluster 1 --voxel_size 0.002 --max_depth 5.0 --use_depth_filter --skip_test"
     cmd = f'CUDA_VISIBLE_DEVICES={gpu_id} python scripts/render_tnt.py -m {out_base_path}/dtu_scan{scene}/{out_name} {common_args}'
